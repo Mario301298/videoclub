b6467a5bd90cdde8aa0fa9ffae93d31cb7bbda73
alumno <alumno@vm1>
2021-01-27 17:32:51 +0100

    Hecho el API en videoclub

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('peliculas', PeliculaController::class);
 Route::get('peliculas/search/{search}', [PeliculaController::class, 'search']);
+
+Route::any('/{any}', function (ServerRequestInterface $request) {
+    $config = new Config([
+        'address' => env('DB_HOST'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'database' => env('DB_DATABASE'),
+        'basePath' => '/api',
+    ]);
+    $api = new Api($config);
+    $response = $api->handle($request);
+    return $response;
+})/*->middleware('auth:sanctum')*/->where('any', '.*');
